08b666c570c7e99b0b9bbffa594b2859a1351f46
duoshine.chen <duoshine.chen@cenhotec.com>
2022-05-24 17:02:16 +0800

    branch4修改，需要合并

--- app/src/main/java/com/cenho/gitapplication/MainActivity.java
+++ app/src/main/java/com/cenho/gitapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //我是Master分支
         //第一次修改
+        //branch4分支修改了逻辑，现在测试正常，需要合并分支
     }
 }
